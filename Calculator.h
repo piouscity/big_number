e29fb8f06e33e07ab2d447ac3b266fc523793bac
Anh-Hao Tran <JATC1024@gmail.com>
2018-03-17 22:11:29 +0700

    Create Calculator.h

---
+++ Calculator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <fstream>
+#include "QInt.h"
+#ifndef _UNEXPECTED_AGRUMENT
+#define _UNEXPECTED_AGRUMENT -1
+#endif
+
+#include <vector>
+using namespace std;
+
+void Calculate(const vector<string> &, ofstream &);
+void Calculate10(const vector<string> &, ofstream &);
+void Calculate2(const vector<string> &, ofstream &);
+void Calculate16(const vector<string> &, ofstream &);
+QInt Calculate(const QInt &, const QInt &, const string &);
